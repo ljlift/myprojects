116b00da3d7fdd9bc2d21200111f72c6e3e754ec
ljli01 <695167222@qq.com>
2018-04-01 21:11:10 +0800

    my first, init!

---
+++ Hello_World.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+'''
+python3.6
+create by ljli
+'''
+
+def hello_world(hw="Hello World!"):
+    print(hw)
+
+if __name__ == "__main__":
+    hello_world()
---
+++ some_leetcode/__init__.py
@@ -0,0 +1,5 @@
+#-*- coding:utf-8 -*-
+'''
+python3.6
+create by ljli
+'''
